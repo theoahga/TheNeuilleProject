1c2296a80452ef779ec916647e4dc8051c362708
Maxime <maxime.brossard5@gmail.com>
2024-01-10 19:54:28 +0100

    Emergency View

---
+++ Emergency-View/src/components/Content.vue
@@ -0,0 +1,51 @@
+<script>
+import Menu from "@/components/Menu.vue";
+import Map from "@/components/Map.vue";
+
+export default {
+  data() {
+    return {
+      positionMap: [45.766914, 4.830913],
+    }
+  },
+  components: {
+    Map,
+    Menu,
+  },
+  methods:{
+    updateStateCaserne : function (e){
+      console.log("Display Caserne State : " + e);
+    },
+    updateStateTruck : function (e){
+      console.log("Display Truck State : " + e);
+    },
+    updateMapPositions : function (e){
+      console.log("New Map Position : " + e)
+      if(e === "Lyon"){
+        this.positionMap = [45.766914, 4.830913];
+      }else if (e === "Villeurbanne"){
+        this.positionMap = [45.783906, 4.869766];
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="wrapper-content">
+    <Menu @change-state-caserne="updateStateCaserne($event)" @change-state-truck="updateStateTruck($event)" @change-map-position="updateMapPositions($event)"/>
+    <Map :center="positionMap"/>
+  </div>
+</template>
+
+<style scoped>
+  .wrapper-content {
+    width: 95%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-left: auto;
+    margin-right: auto;
+  }
+</style>
---
+++ Emergency-View/src/components/Map.vue
@@ -0,0 +1,44 @@
+<script>
+import L from "leaflet";
+import {onMounted} from "vue";
+
+onMounted(() => {
+
+});
+
+export default {
+  data() {
+    return {
+      map:null,
+    }
+  },
+  props: {
+    center: {default:[45.766914, 4.830913]},
+  },
+  mounted() {
+    this.map = L.map('map').setView(this.center, 15);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(this.map);
+  },
+  watch:{
+    center(){
+      this.map.setView(new L.LatLng(this.center[0],this.center[1]),15);
+    }
+  }
+}
+</script>
+
+<template>
+  <div id="map"/>
+</template>
+
+<style scoped>
+#map {
+  width: 65%;
+  height: 85vh;
+  margin-left: auto;
+  border-radius: 5px;
+}
+</style>
