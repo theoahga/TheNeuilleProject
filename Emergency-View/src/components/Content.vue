5293567679417fcc4b310450c77c63e266159465
Maxime Brossard <maxime.brossard5@gmail.com>
2024-01-12 15:14:44 +0100

    Advanced on Emergency-View

--- Emergency-View/src/components/Content.vue
+++ Emergency-View/src/components/Content.vue
@@ -1,11 +1,14 @@
 <script>
 import Menu from "@/components/Menu.vue";
 import Map from "@/components/Map.vue";
+import axios from "axios";
 
 export default {
   data() {
     return {
       positionMap: [45.766914, 4.830913],
+      stationArray: [],
+      sensorArray: [],
     }
   },
   components: {
@@ -15,9 +18,14 @@ export default {
   methods:{
     updateStateCaserne : function (e){
       console.log("Display Caserne State : " + e);
+      this.getStations(e);
     },
     updateStateTruck : function (e){
       console.log("Display Truck State : " + e);
+    },
+    updateStateSensor : function (e){
+      console.log("Display Sensor State : " + e[1]);
+      this.getSensor(e);
     },
     updateMapPositions : function (e){
       console.log("New Map Position : " + e)
@@ -26,6 +34,26 @@ export default {
       }else if (e === "Villeurbanne"){
         this.positionMap = [45.783906, 4.869766];
       }
+    },
+    getStations : async function (event){
+      if(event === true) {
+        await axios.get("http://localhost:10001/emergency/api/station/getAll")
+            .then(response => {
+              this.stationArray = response.data;
+            })
+      }else {
+        this.stationArray = [];
+      }
+    },
+    getSensor : async function (e) {
+      if(e[1] === true) {
+        await axios.get(`http://localhost:10001/emergency/api/sensor/getAll`)
+          .then((response) => {
+            this.sensorArray = response.data;
+          });
+      } else {
+        this.sensorArray = []
+      }
     }
   }
 }
@@ -33,8 +61,8 @@ export default {
 
 <template>
   <div class="wrapper-content">
-    <Menu @change-state-caserne="updateStateCaserne($event)" @change-state-truck="updateStateTruck($event)" @change-map-position="updateMapPositions($event)"/>
-    <Map :center="positionMap"/>
+    <Menu @change-state-caserne="updateStateCaserne($event)" @change-state-truck="updateStateTruck($event)" @change-map-position="updateMapPositions($event)" @change-sensor-truck="updateStateSensor($event)"/>
+    <Map :center="positionMap" :station="stationArray" :sensor="sensorArray"/>
   </div>
 </template>
 
